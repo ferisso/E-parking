57086c4d2b1950aea9dec4803e59d877e4b67d46
Fernando Nogueira Risso <48661787+ferisso@users.noreply.github.com>
2020-01-21 00:46:38 -0300

    Final version of the website.

---
+++ site/site/index.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+  <meta name="generator" content="Jekyll v3.8.5">
+  <title>E-parking</title>
+
+  <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/cover/">
+
+  <!-- Bootstrap core CSS -->
+  <link href="https://getbootstrap.com/docs/4.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+  <style>
+    .bd-placeholder-img {
+      font-size: 1.125rem;
+      text-anchor: middle;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+    }
+
+    @media (min-width: 768px) {
+      .bd-placeholder-img-lg {
+        font-size: 3.5rem;
+      }
+    }
+  </style>
+  <!-- Custom styles for this template -->
+  <link href="css/cover.css" rel="stylesheet">
+</head>
+
+<body class="text-center">
+  <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <header class="masthead mb-auto">
+      <div class="inner">
+        <a class="masthead-brand" href="index.php"><img src="img/logo.png" height="20px"></a>
+        <nav class="nav nav-masthead justify-content-center">
+          <a class="nav-link active" href="Index.php">Home</a>
+          <a class="nav-link" href="precos.php">Planos</a>
+          <a class="nav-link" href="contato.php">Contato</a>
+        </nav>
+      </div>
+    </header>
+
+    <main role="main" class="inner cover">
+      <h1 class="cover-heading">Bem-Vindo ao E-Parking.</h1>
+      <p class="lead">A 1# Multi-Plataforma de reserva de vagas do MUNDO, poupe tempo e dinheiro e venha fazer sua reserva com a gente.</p>
+      <p class="lead">
+        <a href="register.php" class="btn btn-lg btn-secondary">Cadastre-se</a>
+
+      </p>
+    </main>
+
+    <footer class="mastfoot mt-auto">
+      <div class="inner">
+        <a class="nav-link" href="login.php">Já possuo uma conta</a>
+
+      </div>
+    </footer>
+  </div>
+</body>
+
+</html>
---
+++ site/site/server.php
@@ -0,0 +1,96 @@
+<?php
+session_start();
+
+// INICIANDO AS VARIAVEIS 
+$username = "";
+$email    = "";
+$errors = array(); 
+
+// CONNECTANDO COM O DATA BASE
+$db = mysqli_connect('localhost', 'root', '', 'login');
+
+
+
+// REGISTRANDO O USER
+if (isset($_POST['reg_user'])) {
+  // RECEBENDO TODOS OS VALORES DE FORM PARA FORM
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $cpf = mysqli_real_escape_string($db, $_POST['cpf']);
+  $email = mysqli_real_escape_string($db, $_POST['email']);
+  $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+  $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+
+
+
+
+  // FORMULARIO DE VALIDACAO PARA VER SE NAO HA ERROS USANDO UM ARRAY_PUSH()
+
+  if (empty($username)) { array_push($errors, "Coloque o nome de usuario"); }
+  if (empty($email)) { array_push($errors, "Coloque o E-mail"); }
+  if (empty($password_1)) { array_push($errors, "Coloque a senha"); }
+  if ($password_1 != $password_2) {
+	array_push($errors, "As senhas não combinam");
+  }
+
+  // CONSULTA AO DATABASE PARA TER CERTEZA 
+  // SE O USUARIO OU EMAIL NAO EXISTEM
+  $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
+  $result = mysqli_query($db, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+  
+  if ($user) { // VERIFIFICAR SE HA UM USUARIO
+    if ($user['username'] === $username) {
+      array_push($errors, "O nome de usuario ja existe");
+    }
+
+    if ($user['email'] === $email) {
+      array_push($errors, "E-mail ja existe");
+    }
+    
+    if ($user['cpf'] === $cpf) {
+      array_push($errors, "CPF ja existe");
+    }
+
+  }
+
+  // REGISTRO CONCLUIDO CASO NAO HAJA ERROS
+  if (count($errors) == 0) {
+  	$password  = md5($password_1);
+
+  	echo $password ;
+
+    $query = "INSERT INTO users (username, cpf, email, password)
+  			  VALUES('$username','$cpf', '$email', '$password')";
+  	mysqli_query($db, $query);
+  	$_SESSION['username'] = $username;
+  	$_SESSION['success'] = "Bem-vindo";
+  	header('location: index2.php');
+  }
+}
+
+// PAGINA DE LOGIN
+if (isset($_POST['login_user'])) {
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $password = mysqli_real_escape_string($db, $_POST['password']);
+
+  if (empty($username)) {
+    array_push($errors, "Colque o nome de usuario");
+  }
+  if (empty($password)) {
+    array_push($errors, "Coloque a senha");
+  }
+
+  if (count($errors) == 0) {
+    $password = md5($password);
+    $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $results = mysqli_query($db, $query);
+    if (mysqli_num_rows($results) == 1) {
+      $_SESSION['username'] = $username;
+      $_SESSION['success'] = "You are now logged in";
+      header('location: index2.php');
+    }else {
+      array_push($errors, "Senha ou nome de usuraio incorretos");
+    }
+  }
+}
+?>
